cff90a834ac53a28a3ec3101ea5fcf2be9b35a63
CrossFode <149864968+CrossFode@users.noreply.github.com>
2024-06-15 19:49:05 +0700

    first commit

---
+++ Main/Model/Hp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Model
+{
+    internal class Hp
+    {
+        public string MerekHp { get; set; }
+
+        public int KodeHp { get; set; }
+
+        public int JumlahHp { get; set; }
+
+        public int HargaHp { get; set; }
+
+        public Hp(string MerekHp, int KodeHp, int JumlahHp, int HargaHp)
+        {
+            this.MerekHp = MerekHp;
+            this.KodeHp = KodeHp;
+            this.JumlahHp = JumlahHp;
+            this.HargaHp = HargaHp;
+        }
+
+        internal static void Add(Hp hp)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void RemoveAt(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
